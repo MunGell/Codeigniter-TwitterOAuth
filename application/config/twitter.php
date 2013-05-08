d54ae7a1766d22dbc5ff83c95df377d672c11471
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2013-05-08 15:58:12 +0100

    Style corrections, comments

--- application/config/twitter.php
+++ application/config/twitter.php
@@ -1,9 +1,10 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['twitter_consumer_token']		= 'your_consumer_token';
 $config['twitter_consumer_secret']		= 'your_consumer_token_secret';
-$config['twitter_access_token']			= 'your_access_token';
-$config['twitter_access_secret']		= 'your_access_token_secret';
+$config['twitter_access_token']			= 'your_access_token'; // Optional
+$config['twitter_access_secret']		= 'your_access_token_secret'; // Optional
 
 /* End of file twitter.php */
 /* Location: ./application/config/twitter.php */
